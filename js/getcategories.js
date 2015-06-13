6667e4f215d5dc7ae77247d9f2dc2dae066fb612
Anthony Arcidiacono <Anthony@Anthonys-MBP.home>
2015-06-14 09:26:46 +1000

    Getting category JSON

--- js/getcategories.js
+++ js/getcategories.js
@@ -1,7 +1,7 @@
     var app = angular.module('myApp', []);
     app.controller('categoriesCtrl', function($scope, $http) {
-        $http.get("http://www.w3schools.com/angular/customers.php")
+        $http.get("http://gist.githubusercontent.com/anthonyarcidiacono/9ed3af2d52c05380551b/raw/3b22bcb7be2ab4e71b434671910f8420b3d17a10/gistfile1.js")
             .success(function(response) {
-                $scope.names = response.records;
+                $scope.categories = response;
             });
     });
