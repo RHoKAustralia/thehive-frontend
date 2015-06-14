6a79f871bf974be3ed28874c8ddd9bddafc2e660
Anthony Arcidiacono <Anthony@Anthonys-MBP.home>
2015-06-14 12:02:03 +1000

    Added categories from JSON

--- js/getcategories.js
+++ js/getcategories.js
@@ -1,7 +1,12 @@
-    var app = angular.module('myApp', []);
-    app.controller('categoriesCtrl', function($scope, $http) {
-        $http.get("http://gist.githubusercontent.com/anthonyarcidiacono/9ed3af2d52c05380551b/raw/3b22bcb7be2ab4e71b434671910f8420b3d17a10/gistfile1.js")
-            .success(function(response) {
-                $scope.categories = response;
-            });
-    });
+angular.module('getCategories', ['ui.bootstrap']);
+angular.module('getCategories').controller('Accordion', function($scope, $http) {
+    $http.get("http://gist.githubusercontent.com/anthonyarcidiacono/9ed3af2d52c05380551b/raw/3b22bcb7be2ab4e71b434671910f8420b3d17a10/gistfile1.js")
+        .success(function(response) {
+            $scope.categories = response;
+        });
+    $scope.oneAtATime = true;
+    $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
+});
