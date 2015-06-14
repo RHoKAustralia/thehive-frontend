cae10179ffbdf19f572720ddebd1f1a41a188360
Anthony Arcidiacono <Anthony@Anthonys-MBP.home>
2015-06-14 12:31:27 +1000

    Added categories from JSON

--- js/getcategories.js
+++ js/getcategories.js
@@ -1,7 +1,7 @@
 
 angular.module('getCategories', ['ui.bootstrap']);
 angular.module('getCategories').controller('Accordion', function($scope, $http) {
-    $http.get("http://ec2-52-64-56-176.ap-southeast-2.compute.amazonaws.com/")
+    $http.get("http://ec2-52-64-56-176.ap-southeast-2.compute.amazonaws.com/api/category")
         .success(function(response) {
             $scope.categories = response;
         });
