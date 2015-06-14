a3fa589b1fd2c9ed6863650e164ee208e91e45e6
Mario Martinez <zenitram.oiram@gmail.com>
2015-06-14 12:02:35 +1000

    Angular Google Maps api

--- js/getcategories.js
+++ js/getcategories.js
@@ -1,3 +1,4 @@
+
 angular.module('getCategories', ['ui.bootstrap']);
 angular.module('getCategories').controller('Accordion', function($scope, $http) {
     $http.get("http://gist.githubusercontent.com/anthonyarcidiacono/9ed3af2d52c05380551b/raw/3b22bcb7be2ab4e71b434671910f8420b3d17a10/gistfile1.js")
@@ -11,9 +12,16 @@ angular.module('getCategories').controller('Accordion', function($scope, $http) 
     };
 });
 
+var app = angular.module('myApp', []);
+
+    angular.module('myApp', ['uiGmapgoogle-maps']);
+
     app.controller('resultsCtrl', function($scope, $http) {
         $http.get("https://gist.githubusercontent.com/martimatix/8fdbbcd613cc9b228144/raw/066f120ea50c5d0496f163cec00701cf7b87b7f9/results.json")
             .success(function(response) {
                 $scope.results = response;
+                var lat = $scope.results[0].locations[0].latitude;
+                var lng = $scope.results[0].locations[0].longitude;
+                $scope.map = { center: { latitude: lat, longitude: lng }, zoom: 8 };
             });
     });
