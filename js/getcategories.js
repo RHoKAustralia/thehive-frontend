9d09f11586ff87f2efc760b446e8543cd1c3fbd0
Anthony Arcidiacono <Anthony@Anthonys-MBP.home>
2015-06-14 12:21:49 +1000

    Added categories from JSON

--- js/getcategories.js
+++ js/getcategories.js
@@ -1,6 +1,6 @@
 angular.module('getCategories', ['ui.bootstrap']);
 angular.module('getCategories').controller('Accordion', function($scope, $http) {
-    $http.get("http://ec2-52-64-56-176.ap-southeast-2.compute.amazonaws.com/api/category")
+    $http.get("http://gist.githubusercontent.com/anthonyarcidiacono/9ed3af2d52c05380551b/raw/3b22bcb7be2ab4e71b434671910f8420b3d17a10/gistfile1.js")
         .success(function(response) {
             $scope.categories = response;
         });
